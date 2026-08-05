86610a352e19806f3e7f62726754294c88231ff9
ayushjangir <100561425+ayushjangir@users.noreply.github.com>
2026-08-05 19:29:27 +0530

    Add files via upload

---
+++ arrays/1-set_matrix_zeroes.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int col0 = 1;
+
+    // First pass to mark the rows and columns that need to be zeroed
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==0){
+                matrix[i][0] = 0;
+                if(j!=0){
+                    matrix[0][j] = 0;
+                }
+                else{
+                    col0 = 0;
+                }
+            }
+        }
+
+    }
+
+    // Second pass to set the zeroes based on the markers
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(matrix[i][j]!=0){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j] = 0;
+                }
+
+            }
+        }
+    }
+
+    // Finally, set the first row and first column to zero if needed
+
+    if(matrix[0][0]==0){
+        for(int j=0;j<m;j++){
+            matrix[0][j] = 0;
+        }
+    }
+
+
+    if(col0==0){
+        for(int i=0;i<n;i++){
+            matrix[i][0] = 0;
+        }
+    }
+
+    }
+};
