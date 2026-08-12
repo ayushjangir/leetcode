31c7b40c8d0d3966547a6df5b86e0878557f78f9
Ayush Jangir <100561425+ayushjangir@users.noreply.github.com>
2026-08-12 23:41:50 +0530

    Implement merge function for two sorted arrays

---
+++ arrays/9-merge-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int left=0,right=0,index=0;
+        vector <int> arr(n+m);
+        while(left<m && right<n){
+            if(nums1[left]<=nums2[right]){
+                arr[index]=nums1[left];
+                left++;index++;
+            }
+            else{
+                arr[index]=nums2[right];
+                index++;right++;
+            }
+        }
+
+        while(left<m){
+            arr[index]=nums1[left];
+            left++;index++;
+        }
+
+        while(right<n){
+            arr[index]=nums2[right];
+            index++;right++;
+        }
+
+        for(int i=0;i<m+n;i++){
+                nums1[i]=arr[i];
+         }
+    }
+};
