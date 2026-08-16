be08d3997a20495bc7f0ef6030e0e142e78370b6
Ayush Jangir <100561425+ayushjangir@users.noreply.github.com>
2026-08-16 23:44:57 +0530

    Create 14-majority_element2.cpp

---
+++ arrays/14-majority_element2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1=0,cnt2=0;
+        int el1=INT_MIN,el2=INT_MIN;
+        
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1++;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2++;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1){
+                cnt1++;
+            }
+            else if(nums[i]==el2){
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        // manual check
+
+        vector<int> ls;
+        cnt1=0, cnt2=0;
+        for(int i=0; i<nums.size();i++){
+            if(nums[i]==el1) cnt1++;
+            if(nums[i]==el2) cnt2++;
+        }
+
+        // mini is the minimum no of times the repeating( > n/3) element can occur
+    
+        int mini = (nums.size()/3)+1;
+        
+        if(cnt1>=mini) ls.push_back(el1);
+        if(cnt2>=mini) ls.push_back(el2);
+        
+        sort(ls.begin(),ls.end());
+        return ls;
+
+    }
+};
